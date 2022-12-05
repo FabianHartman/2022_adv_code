4a5ef2133b9b979934b21ebc3de31aad39c942bd
FabianHartman <fabian.hartman@student.hu.nl>
2022-12-05 19:44:40 +0100

    day 2

---
+++ day 2/adv_of_code_2022_day_2_part_1.py
@@ -0,0 +1,27 @@
+databestand = open('adv_of_code_2022_day_2_data.txt','r')
+rondes = databestand.read().splitlines()
+score = int()
+for ronde in rondes:
+    print(ronde)
+    if ronde[0]=='A':
+        if ronde[2]=='X':
+            score+=4
+        if ronde[2] == 'Y':
+            score+=8
+        if ronde[2]=='Z':
+            score+=3
+    if ronde[0]=='B':
+        if ronde[2]=='X':
+            score+=1
+        if ronde[2]=='Y':
+            score+=5
+        if ronde[2] == 'Z':
+            score+=9
+    if ronde[0]=='C':
+        if ronde[2]=='X':
+            score+=7
+        if ronde[2] == 'Y':
+            score+=2
+        if ronde[2] == 'Z':
+            score+=6
+print(score)
